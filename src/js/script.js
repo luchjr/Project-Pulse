2ecbaf451dbbb7c31e36b824ae6642762af321f8
luch <rockwindsea@gmail.com>
2022-05-20 10:15:49 +0300

    markup finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //карусель slick https://kenwheeler.github.io/slick/
     $('.carousel__inner').slick({
         infinite: true,
         speed: 1200,
@@ -48,6 +49,7 @@ $(document).ready(function(){
     */
 
     /* оптимизированный вариант */
+    
     function toggleSlide(item) {
         $(item).each(function(i) {
             $(this).on('click', function(e) {
@@ -80,5 +82,97 @@ $(document).ready(function(){
             $('.overlay, #order').fadeIn('slow');
         })
     });
+
+    //валидатор форм https://jqueryvalidation.org/
+    
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Введите имя",
+                    minlength: jQuery.validator.format("Введите {0} символов")
+                  },
+                phone: "Пожалуйста введите номер телефона",
+                email: {
+                    required: "Введите свою почту",
+                    email: "Неправильно введен адрес почты"
+                } 
+              }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    // маска ввода номера https://plugins.jquery.com/maskedinput/
+
+    $('input[name=phone]').mask("+0 (999) 999-9999"); 
+
+    //отправка данных форм на почту + см. mailer/smart.php
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        if (!$(this).valid()) {
+            return;
+        }
+        $.ajax({
+            type: "POST",
+            url: "js/mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    //плавный скролл
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+    
+          // Store hash
+          var hash = this.hash;
+    
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 200, function(){
+    
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+      });
+
+      // подключение wow.js чтобы анимация animate.css срабатывала когда долистываешь страницу до элемента
+      new WOW().init();    
 });
 
